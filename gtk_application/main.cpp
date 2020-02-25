eb9bafa76a0e006f11b60d5035f540c5d6419d25
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-02-25 15:07:21 -0300

    Add files via upload

--- gtk_application/main.cpp
+++ gtk_application/main.cpp
@@ -5,13 +5,13 @@ GtkWidget* window;
 
 static void GtkOnActivate(GtkApplication *app) 
 {
-  window = gtk_application_window_new (app);
-  gtk_widget_show_all (window);
+  window = gtk_application_window_new(app);
+  gtk_widget_show_all(window);
 }
 
 int main (int argc, char** argv)
 {
   application = gtk_application_new ("null.null.GtkApplication", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(application, "activate", G_CALLBACK (GtkOnActivate), NULL);
-  return g_application_run((GApplication*)application, argc, argv);
+  g_signal_connect(application, "activate", G_CALLBACK(GtkOnActivate), NULL);
+  return g_application_run(G_APPLICATION(application), argc, argv);
 }
